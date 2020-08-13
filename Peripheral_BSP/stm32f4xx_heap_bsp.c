e5b35a682710dd9a107693b78242a17f77aa0866
Lanceli <47838732+laneston@users.noreply.github.com>
2020-08-13 17:17:35 +0800

    20200813-1

--- Peripheral_BSP/stm32f4xx_heap_bsp.c
+++ Peripheral_BSP/stm32f4xx_heap_bsp.c
@@ -21,96 +21,98 @@
 
 /*
 *function: Initialize SRAM.
-*param: p,				object of Heap_TypeDef Struct.
+*param: p,		    object of Heap_TypeDef Struct.
 *param: heapHead,	the head address of SRAM.
+
 *param: heapSize, the size of SRAM that can be use.
 */
 void stSramInit(Heap_TypeDef *p, u32 headAddr, u32 heapSize)
 {
-	u32 offset = 0;
+    u32 offset = 0;
 #ifdef HEAP_DEBUG
-	UART_Init_115200();
+    UART_Init_115200();
 #endif
-	offset = (u32)(headAddr%ALIGNMENT_SIZE);
+    offset = (u32)(headAddr%ALIGNMENT_SIZE);
 #ifdef HEAP_DEBUG
-	printf("offset:%d\r\n",offset);
+    printf("offset:%d\r\n",offset);
 #endif
-	/*alignment*/
-	if(offset)
-	{
-		p->heap_head = (void *)(headAddr + ALIGNMENT_SIZE - offset);
-	}
-	else
-	{
-		p->heap_head = (void *)headAddr;
-	}
+    /*alignment*/
+    if(offset)
+    {
+        p->heap_head = (void *)(headAddr + ALIGNMENT_SIZE - offset);
+    }
+    else
+    {
+        p->heap_head = (void *)headAddr;
+    }
 #ifdef HEAP_DEBUG
-	printf("heapHead:%p\r\n",p->heap_head);
+    printf("heapHead:%p\r\n",p->heap_head);
 #endif
-	
-	offset = (headAddr + heapSize)%ALIGNMENT_SIZE;
+    
+    offset = (headAddr + heapSize)%ALIGNMENT_SIZE;
 #ifdef HEAP_DEBUG
-	printf("offset:%d\r\n",offset);
+    printf("offset:%d\r\n",offset);
 #endif
-	if(offset)
-	{
-		p->heap_tail = (void *)(headAddr + heapSize - offset);
-	}
-	else
-	{
-		p->heap_tail = (void *)(headAddr + heapSize);
-	}
+    if(offset)
+    {
+        p->heap_tail = (void *)(headAddr + heapSize - offset);
+    }
+    else
+    {
+        p->heap_tail = (void *)(headAddr + heapSize);
+    }
 #ifdef HEAP_DEBUG
-	printf("heapTail:%p\r\n",p->heap_tail);
+    printf("heapTail:%p\r\n",p->heap_tail);
 #endif	
-	
-	p->heap_size = ((u32)((u8 *)p->heap_tail - (u8 *)p->heap_head));
+    
+    p->heap_size = ((u32)((u8 *)p->heap_tail - (u8 *)p->heap_head));
 #ifdef HEAP_DEBUG
-	printf("heap_size:%d\r\n",p->heap_size);
+    printf("heap_size:%d\r\n",p->heap_size);
 #endif
-	
-	p->heap_current = p->heap_head;
-	p->heap_residual = p->heap_size;
+    
+    p->heap_current = p->heap_head;
+    p->heap_residual = p->heap_size;
 #ifdef HEAP_DEBUG
-	printf("heap_residual:%d\r\nheap_current:%p\r\n",p->heap_residual,p->heap_current);
+    printf("heap_residual:%d\r\nheap_current:%p\r\n",p->heap_residual,p->heap_current);
 #endif
 }
 
 /*
 *function: Allocate memory.
-*param: p,				    object of Heap_TypeDef Struct.
+*param: p,				object of Heap_TypeDef Struct.
 *param: xWantedSize,	the size you need.
+
 *return: the pointer of the memory.
 */
 void *stSramMalloc(Heap_TypeDef *p, u32 xWantedSize )
 {
-	void *pvReturn = NULL;
-	u32 offset, current_offset;
-	
-	if(xWantedSize)
-	{
-		pvReturn = p->heap_current;	
-	  offset = xWantedSize%ALIGNMENT_SIZE;
+    void *pvReturn = NULL;
+    u32 offset, current_offset;
+    
+    if(xWantedSize)
+    {
+        pvReturn = p->heap_current;	
+      offset = xWantedSize%ALIGNMENT_SIZE;
 #ifdef HEAP_DEBUG
     printf("offset:%d\r\n",offset);
 #endif		
-		if(offset)
-		{
-			current_offset = xWantedSize + ALIGNMENT_SIZE - offset;
-			p->heap_current = (void *)((u8 *)p->heap_current + current_offset);
-		}
-		else
-		{
-			p->heap_current = (void *)((u8 *)p->heap_current + xWantedSize);
-		}
-		p->heap_residual = (u32)((u8 *)p->heap_tail - (u8 *)p->heap_current);
+        if(offset)
+        {
+            current_offset = xWantedSize + ALIGNMENT_SIZE - offset;
+            p->heap_current = (void *)((u8 *)p->heap_current + current_offset);
+        }
+        else
+        {
+            p->heap_current = (void *)((u8 *)p->heap_current + xWantedSize);
+        }
+        p->heap_residual = (u32)((u8 *)p->heap_tail - (u8 *)p->heap_current);
 #ifdef HEAP_DEBUG
-	  printf("heap_residual:%d\r\nheap_current:%p\r\n",p->heap_residual,p->heap_current);
+      printf("heap_residual:%d\r\nheap_current:%p\r\n",p->heap_residual,p->heap_current);
 #endif
-	}
-	else
-	{
-		pvReturn = NULL;
-	}	
-	return pvReturn;
+    }
+    else
+    {
+        pvReturn = NULL;
+    }	
+    return pvReturn;
 }
--- Peripheral_BSP/stm32f4xx_uart_bsp.c
+++ Peripheral_BSP/stm32f4xx_uart_bsp.c
@@ -22,17 +22,17 @@
 
 #ifdef	UART_IT
 
-	#ifdef UART_IT_IDLE
-	xSemaphoreHandle Semaphore_uart_idle = NULL;
-	#endif
+    #ifdef UART_IT_IDLE
+    xSemaphoreHandle Semaphore_uart_idle = NULL;
+    #endif
 
-	#ifdef UART_IT_TC
-	xSemaphoreHandle Semaphore_uart_tc = NULL;
-	#endif
+    #ifdef UART_IT_TC
+    xSemaphoreHandle Semaphore_uart_tc = NULL;
+    #endif
 
-	#ifdef	UART_DMA_IT
-	xSemaphoreHandle Semaphore_uart_dma = NULL;
-	#endif
+    #ifdef	UART_DMA_IT
+    xSemaphoreHandle Semaphore_uart_dma = NULL;
+    #endif
 
 #endif
 
@@ -45,7 +45,7 @@ uint8_t *RxBuffer1;
 uint8_t *TxBuffer;
 
 struct __FILE {
-	int handle; 
+    int handle; 
 }; 
 FILE __stdout;
 void _sys_exit(int x) {	x = x;}
@@ -58,19 +58,19 @@ void _sys_exit(int x) {	x = x;}
   */
 static void USART_Pins(void)
 {
-	GPIO_InitTypeDef GPIO_InitStructure;
-	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
-	
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1);
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1);
-	
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+    
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1);
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1);
+    
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOA,&GPIO_InitStructure);
 }
 
 /**
@@ -81,40 +81,40 @@ static void USART_Pins(void)
 #ifdef	UART_IT
 static void USART_NVIC(void)
 {
-	NVIC_InitTypeDef NVIC_InitStructure;
-	
-	#ifdef	UART_IT_RXNE
-		USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-	#endif
-	#ifdef	UART_IT_IDLE
-		USART_ITConfig(USART1, USART_IT_IDLE, ENABLE);
-	#endif	
-	#ifdef	UART_IT_TC
-		USART_ITConfig(USART1, USART_IT_TC, ENABLE);
-	#endif
+    NVIC_InitTypeDef NVIC_InitStructure;
+    
+    #ifdef	UART_IT_RXNE
+        USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+    #endif
+    #ifdef	UART_IT_IDLE
+        USART_ITConfig(USART1, USART_IT_IDLE, ENABLE);
+    #endif	
+    #ifdef	UART_IT_TC
+        USART_ITConfig(USART1, USART_IT_TC, ENABLE);
+    #endif
 
-	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=USART1_IRQnPriority;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority =0;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=USART1_IRQnPriority;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority =0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
 }
 
-	#ifdef	UART_DMA_IT
-	static void DMA_UART_NVIC(void)
-	{
-		NVIC_InitTypeDef NVIC_InitStructure;
+    #ifdef	UART_DMA_IT
+    static void DMA_UART_NVIC(void)
+    {
+        NVIC_InitTypeDef NVIC_InitStructure;
 
-		DMA_ClearITPendingBit(DMA2_Stream5, DMA_IT_TCIF5);
-		DMA_ITConfig(DMA2_Stream5, DMA_IT_TC, ENABLE);
+        DMA_ClearITPendingBit(DMA2_Stream5, DMA_IT_TCIF5);
+        DMA_ITConfig(DMA2_Stream5, DMA_IT_TC, ENABLE);
 
-		NVIC_InitStructure.NVIC_IRQChannel = DMA2_Stream5_IRQn;
-		NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=DMA2_Stream5_IRQnPriority;
-		NVIC_InitStructure.NVIC_IRQChannelSubPriority =0;
-		NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-		NVIC_Init(&NVIC_InitStructure);
-	}
-	#endif
+        NVIC_InitStructure.NVIC_IRQChannel = DMA2_Stream5_IRQn;
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=DMA2_Stream5_IRQnPriority;
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority =0;
+        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+        NVIC_Init(&NVIC_InitStructure);
+    }
+    #endif
 #endif	
 
 
@@ -126,7 +126,7 @@ static void USART_NVIC(void)
 void USART_Config(u32 boundrate, u16 StopBits, u16 Parity, u16 HardwareFlowControl)
 {		
   USART_InitTypeDef USART_InitStructure;
-	
+    
   
   /* USARTx configured as follows:
         - BaudRate = 115200 baud  
@@ -136,17 +136,17 @@ void USART_Config(u32 boundrate, u16 StopBits, u16 Parity, u16 HardwareFlowContr
         - Hardware flow control disabled (RTS and CTS signals)
         - Receive and transmit enabled
   */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);	
-	
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);	
+    
   USART_InitStructure.USART_BaudRate = boundrate;
   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
   USART_InitStructure.USART_StopBits = StopBits;
   USART_InitStructure.USART_Parity = Parity;
   USART_InitStructure.USART_HardwareFlowControl = HardwareFlowControl;
   USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_Init(USART1, &USART_InitStructure);	
-	
-	USART_Cmd(USART1, ENABLE);
+    USART_Init(USART1, &USART_InitStructure);	
+    
+    USART_Cmd(USART1, ENABLE);
 }
 
 
@@ -157,11 +157,11 @@ void USART_Config(u32 boundrate, u16 StopBits, u16 Parity, u16 HardwareFlowContr
   */
 void USART_MDA_Config(void)
 {
-	DMA_InitTypeDef  DMA_InitStructure;
-	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);	
-	
-	/* Configure DMA Initialization Structure */
+    DMA_InitTypeDef  DMA_InitStructure;
+    
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);	
+    
+    /* Configure DMA Initialization Structure */
   DMA_InitStructure.DMA_BufferSize = UART_RX_BUFFER_SIZE;
   DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable ;
   DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_1QuarterFull ;
@@ -174,32 +174,32 @@ void USART_MDA_Config(void)
   DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
   DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
   DMA_InitStructure.DMA_Priority = DMA_Priority_High;
-	
+    
   /* Configure RX DMA */
   DMA_InitStructure.DMA_Channel = DMA_Channel_4;
   DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory ;
   DMA_InitStructure.DMA_Memory0BaseAddr = (u32)RxBuffer0;
   DMA_Init(DMA2_Stream5, &DMA_InitStructure);
-	
-	DMA_DoubleBufferModeConfig(DMA2_Stream5, (uint32_t)RxBuffer1, DMA_Memory_0);
-	DMA_DoubleBufferModeCmd(DMA2_Stream5, ENABLE);
+    
+    DMA_DoubleBufferModeConfig(DMA2_Stream5, (uint32_t)RxBuffer1, DMA_Memory_0);
+    DMA_DoubleBufferModeCmd(DMA2_Stream5, ENABLE);
 
   /* Configure TX DMA */
-	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-	DMA_InitStructure.DMA_BufferSize = UART_TX_BUFFER_SIZE;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_BufferSize = UART_TX_BUFFER_SIZE;
   DMA_InitStructure.DMA_Channel = DMA_Channel_4;
   DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
   DMA_InitStructure.DMA_Memory0BaseAddr =(uint32_t)TxBuffer;
   DMA_Init(DMA2_Stream7,&DMA_InitStructure);
-	
+    
   /* Enable DMA USART RX Stream */
-	DMA_Cmd(DMA2_Stream5,ENABLE);
+    DMA_Cmd(DMA2_Stream5,ENABLE);
   /* Enable DMA USART TX Stream */
 //  DMA_Cmd(DMA2_Stream7,ENABLE);
-	
-	USART_DMACmd(USART1, USART_DMAReq_Rx, ENABLE);
-	
-	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
+    
+    USART_DMACmd(USART1, USART_DMAReq_Rx, ENABLE);
+    
+    USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
 }
 
 
@@ -210,129 +210,129 @@ void USART_MDA_Config(void)
   */
 void UART_Init_115200(void)
 {
-	USART_Pins();
-	USART_Config(UART_BAUDRATE, UART_STOPBITS, UART_PARITY, UART_FLOWCONTROL);
+    USART_Pins();
+    USART_Config(UART_BAUDRATE, UART_STOPBITS, UART_PARITY, UART_FLOWCONTROL);
 }
 
 void UART_Init(u32 boundrate, u16 StopBits, u16 Parity, u16 HardwareFlowControl)
 {
-	USART_Pins();
+    USART_Pins();
   /* USARTx configuration ----------------------------------------------------*/
   /* Enable the USART OverSampling by 8 */
-	
+    
 #ifdef	UART_OverSampling
   USART_OverSampling8Cmd(USART1, ENABLE);
 #endif
   /* USARTx configured as follows:
         - BaudRate = 5250000 baud
-		   - Maximum BaudRate that can be achieved when using the Oversampling by 8
-		     is: (USART APB Clock / 8) 
-			 Example: 
-			    - (USART3 APB1 Clock / 8) = (42 MHz / 8) = 5250000 baud
-			    - (USART1 APB2 Clock / 8) = (84 MHz / 8) = 10500000 baud
-		   - Maximum BaudRate that can be achieved when using the Oversampling by 16
-		     is: (USART APB Clock / 16) 
-			 Example: (USART3 APB1 Clock / 16) = (42 MHz / 16) = 2625000 baud
-			 Example: (USART1 APB2 Clock / 16) = (84 MHz / 16) = 5250000 baud
+           - Maximum BaudRate that can be achieved when using the Oversampling by 8
+             is: (USART APB Clock / 8) 
+             Example: 
+                - (USART3 APB1 Clock / 8) = (42 MHz / 8) = 5250000 baud
+                - (USART1 APB2 Clock / 8) = (84 MHz / 8) = 10500000 baud
+           - Maximum BaudRate that can be achieved when using the Oversampling by 16
+             is: (USART APB Clock / 16) 
+             Example: (USART3 APB1 Clock / 16) = (42 MHz / 16) = 2625000 baud
+             Example: (USART1 APB2 Clock / 16) = (84 MHz / 16) = 5250000 baud
         - Word Length = 8 Bits
         - one Stop Bit
         - No parity
         - Hardware flow control disabled (RTS and CTS signals)
         - Receive and transmit enabled
   */ 
-	USART_Config(boundrate, StopBits, Parity, HardwareFlowControl);
-	USART_MDA_Config();
+    USART_Config(boundrate, StopBits, Parity, HardwareFlowControl);
+    USART_MDA_Config();
 #ifdef	UART_IT
 
-	USART_NVIC();	
+    USART_NVIC();	
 
-	#ifdef	UART_IT_IDLE
-		if (Semaphore_uart_idle == NULL)
-		{
-			vSemaphoreCreateBinary(Semaphore_uart_idle);
-			xSemaphoreTake( Semaphore_uart_idle, 0);
-		}
-	#endif
-		
-	#ifdef	UART_IT_TC
-		if (Semaphore_uart_tc == NULL)
-		{
-			vSemaphoreCreateBinary(Semaphore_uart_tc);
-			xSemaphoreTake( Semaphore_uart_tc, 0);
-		}
-	#endif
-		
-	#ifdef	UART_DMA_IT
-		DMA_UART_NVIC();
-		if (Semaphore_uart_dma == NULL)
-		{
-			vSemaphoreCreateBinary(Semaphore_uart_dma);
-			xSemaphoreTake( Semaphore_uart_dma, 0);
-		}
-	#endif
-		
+    #ifdef	UART_IT_IDLE
+        if (Semaphore_uart_idle == NULL)
+        {
+            vSemaphoreCreateBinary(Semaphore_uart_idle);
+            xSemaphoreTake( Semaphore_uart_idle, 0);
+        }
+    #endif
+        
+    #ifdef	UART_IT_TC
+        if (Semaphore_uart_tc == NULL)
+        {
+            vSemaphoreCreateBinary(Semaphore_uart_tc);
+            xSemaphoreTake( Semaphore_uart_tc, 0);
+        }
+    #endif
+        
+    #ifdef	UART_DMA_IT
+        DMA_UART_NVIC();
+        if (Semaphore_uart_dma == NULL)
+        {
+            vSemaphoreCreateBinary(Semaphore_uart_dma);
+            xSemaphoreTake( Semaphore_uart_dma, 0);
+        }
+    #endif
+        
 #endif
 }
 
 #ifdef	UART_IT
 
-	#ifdef	UART_IT_RXNE
-	void UartRecv_RXNE_IRQ(void)
-	{
-		RxdBufferStructure.uartRecv_Counter++;
-	}
-	#endif
+    #ifdef	UART_IT_RXNE
+    void UartRecv_RXNE_IRQ(void)
+    {
+        RxdBufferStructure.uartRecv_Counter++;
+    }
+    #endif
 
-	#ifdef	UART_IT_IDLE
-	void UartRecv_IDLE_IRQ(void)
-	{
-		portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+    #ifdef	UART_IT_IDLE
+    void UartRecv_IDLE_IRQ(void)
+    {
+        portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
 
-		/* Give the semaphore to wakeup LwIP task */
-		xSemaphoreGiveFromISR( Semaphore_uart_idle, &xHigherPriorityTaskWoken );
-		
-		/* Switch tasks if necessary. */
-		if( xHigherPriorityTaskWoken != pdFALSE )
-		{
-			portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
-		}
-	}
-	#endif
-	
-	#ifdef	UART_IT_TC
-	void UartTransmit_IRQ(void)
-	{
-		portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+        /* Give the semaphore to wakeup LwIP task */
+        xSemaphoreGiveFromISR( Semaphore_uart_idle, &xHigherPriorityTaskWoken );
+        
+        /* Switch tasks if necessary. */
+        if( xHigherPriorityTaskWoken != pdFALSE )
+        {
+            portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
+        }
+    }
+    #endif
+    
+    #ifdef	UART_IT_TC
+    void UartTransmit_IRQ(void)
+    {
+        portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
 
-		/* Give the semaphore to wakeup LwIP task */
-		xSemaphoreGiveFromISR( Semaphore_uart_tc, &xHigherPriorityTaskWoken );
-		
-		/* Switch tasks if necessary. */
-		if( xHigherPriorityTaskWoken != pdFALSE )
-		{
-			portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
-		}
-	}
-	#endif
+        /* Give the semaphore to wakeup LwIP task */
+        xSemaphoreGiveFromISR( Semaphore_uart_tc, &xHigherPriorityTaskWoken );
+        
+        /* Switch tasks if necessary. */
+        if( xHigherPriorityTaskWoken != pdFALSE )
+        {
+            portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
+        }
+    }
+    #endif
 
-	#ifdef UART_DMA_IT
-	void DmaComplete_IRQ(void)
-	{
-		portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
-		RxdBufferStructure.dmaReversalValue = !RxdBufferStructure.dmaReversalValue;
-		RxdBufferStructure.dmaCompleteCounter = RxdBufferStructure.uartRecv_Counter;
+    #ifdef UART_DMA_IT
+    void DmaComplete_IRQ(void)
+    {
+        portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+        RxdBufferStructure.dmaReversalValue = !RxdBufferStructure.dmaReversalValue;
+        RxdBufferStructure.dmaCompleteCounter = RxdBufferStructure.uartRecv_Counter;
 
-		/* Give the semaphore to wakeup LwIP task */
-		xSemaphoreGiveFromISR( Semaphore_uart_dma, &xHigherPriorityTaskWoken );
-		
-		/* Switch tasks if necessary. */
-		if( xHigherPriorityTaskWoken != pdFALSE )
-		{
-			portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
-		}
-	}
-	#endif
-	
+        /* Give the semaphore to wakeup LwIP task */
+        xSemaphoreGiveFromISR( Semaphore_uart_dma, &xHigherPriorityTaskWoken );
+        
+        /* Switch tasks if necessary. */
+        if( xHigherPriorityTaskWoken != pdFALSE )
+        {
+            portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
+        }
+    }
+    #endif
+    
 #endif
 
 
@@ -345,7 +345,7 @@ PUTCHAR_PROTOTYPE
 {
   /* Place your implementation of fputc here */
   /* e.g. write a character to the USART */
-	USART1->SR;
+    USART1->SR;
   USART_SendData(USART1, (uint8_t) ch);
 
   /* Loop until the end of transmission */
